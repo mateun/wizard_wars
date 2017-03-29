47e38e9c5f4edfd8f32d3910fe24bb1457324370
Martin Gruscher <m.gruscher@ttech.at>
2017-03-29 21:02:33 +0200

    first basic (ugly...) hexfield rendering

--- OpenGLTest1/WizardWarsGameWindow.cs
+++ OpenGLTest1/WizardWarsGameWindow.cs
@@ -15,6 +15,18 @@ namespace OpenGLTest1
 {
     class WizardWarsGameWindow : OpenTK.GameWindow
     {
+
+        enum HexTriangleType
+        {
+            Top, 
+            TopRight, 
+            TopLeft, 
+            BotLeft, 
+            BotRight, 
+            Bot
+        }
+
+
         public WizardWarsGameWindow() : base(800, 600, GraphicsMode.Default, "WizardWars 0.0.1")
         {
             VSync = VSyncMode.On;
@@ -23,6 +35,46 @@ namespace OpenGLTest1
         int textureIdHouse = 0;
         int textureIdCastle = 0;
 
+        float outerRadius()
+        {
+            return 1;
+            
+        }
+
+        float innerRadius()
+        {
+            return (float)Math.Sqrt(3) / 2 * outerRadius();
+        }
+
+        Vector3[] getHexVectors()
+        {
+            Vector3[] vecs = new Vector3[7];
+
+            vecs[0] =new Vector3(0, 0, 0);
+            vecs[1] = new Vector3(0.5f * outerRadius(), innerRadius(), 0);
+            vecs[2] = new Vector3(-0.5f * outerRadius(), innerRadius(), 0);
+            vecs[3] = new Vector3(-outerRadius(), 0, 0);
+            vecs[4] = new Vector3(-0.5f * outerRadius(), -innerRadius(), 0);
+            vecs[5] = new Vector3(0.5f * outerRadius(), -innerRadius(), 0);
+            vecs[6] = new Vector3(outerRadius(), 0, 0);
+            
+            return vecs;
+        }
+
+        int[] getHexTriangleIndices(HexTriangleType hexTriangleType)
+        {
+            switch (hexTriangleType)
+            {
+                case HexTriangleType.Top: return new int[3]{ 0, 1, 2};
+                case HexTriangleType.TopRight: return new int[3] { 0, 6, 1 };
+                case HexTriangleType.TopLeft: return new int[3] { 0, 2, 3 };
+                case HexTriangleType.BotLeft: return new int[3] { 0, 3, 4 };
+                case HexTriangleType.Bot: return new int[3] { 0, 4, 5 };
+                case HexTriangleType.BotRight: return new int[3] { 0, 5, 6 };
+                default: return null;
+            }
+        }
+        
         protected override void OnLoad(EventArgs e)
         {
             base.OnLoad(e);
@@ -81,6 +133,30 @@ namespace OpenGLTest1
             GL.End();
         }
 
+        protected void drawHex(Vector3 position, Vector3 color)
+        {
+            Vector3[] hexVectors = getHexVectors();
+            int[] top = getHexTriangleIndices(HexTriangleType.Top);
+            int[] topRight = getHexTriangleIndices(HexTriangleType.TopRight);
+            int[] topLeft = getHexTriangleIndices(HexTriangleType.TopLeft);
+            int[] botLeft = getHexTriangleIndices(HexTriangleType.BotLeft);
+            int[] bot = getHexTriangleIndices(HexTriangleType.Bot);
+            int[] botRight = getHexTriangleIndices(HexTriangleType.BotRight);
+
+            for (int i = 0; i < 3; i++) { GL.Color3(color); GL.Vertex3((position + (hexVectors[top[i]])).X, (position + hexVectors[top[i]]).Y, (position + hexVectors[top[i]]).Z); }
+            
+            for (int i = 0; i < 3; i++) { GL.Color3(color); GL.Vertex3((position + hexVectors[topRight[i]]).X, (position + hexVectors[topRight[i]]).Y, (position + hexVectors[topRight[i]]).Z); }
+
+            for (int i = 0; i < 3; i++) { GL.Color3(color); GL.Vertex3((position + hexVectors[topLeft[i]]).X, (position + hexVectors[topLeft[i]]).Y, (position + hexVectors[topLeft[i]]).Z); }
+
+            for (int i = 0; i < 3; i++) { GL.Color3(color); GL.Vertex3((position + hexVectors[botLeft[i]]).X, (position + hexVectors[botLeft[i]]).Y, (position + hexVectors[botLeft[i]]).Z); }
+
+            for (int i = 0; i < 3; i++) { GL.Color3(color); GL.Vertex3((position + hexVectors[bot[i]]).X, (position + hexVectors[bot[i]]).Y, (position + hexVectors[bot[i]]).Z); }
+
+            for (int i = 0; i < 3; i++) { GL.Color3(color); GL.Vertex3((position + hexVectors[botRight[i]]).X, (position + hexVectors[botRight[i]]).Y, (position + hexVectors[botRight[i]]).Z); }
+
+        }
+        
         protected override void OnRenderFrame(FrameEventArgs e)
         {
             base.OnRenderFrame(e);
@@ -91,57 +167,54 @@ namespace OpenGLTest1
             GL.MatrixMode(MatrixMode.Modelview);
             GL.LoadMatrix(ref modelview);
 
+          
+
+            GL.PolygonMode(MaterialFace.FrontAndBack, PolygonMode.Line);
 
             // Draw a single hex in the center of the screen
             GL.Begin(BeginMode.Triangles);
-            for (int x = 0; x < 3; x++)
+
+            // Just draw one hex
+            // 1st row, 1st col
+            Vector3 white = new Vector3(1, 1, 1);
+            //drawHex(new Vector3(0 * outerRadius(), 0, 0), white);
+            //drawHex(new Vector3(1.5f * outerRadius(), -innerRadius(), 0), white);
+            
+            //drawHex(new Vector3(3f * outerRadius(), 0, 0), white);
+            //drawHex(new Vector3(0 * outerRadius(), 2f* -innerRadius(), 0), white);
+            //drawHex(new Vector3(3f * outerRadius(), 2f * -innerRadius(), 0), white);
+
+            Vector3 vertOffset = new Vector3(-12, 8, 0);
+            Vector3 baseOffset = new Vector3(1.5f, 0, 0);
+
+            float downVal = 0;
+            for (int row = 0; row < 45; row++)
             {
-                for (int y=0; y < 3; y++)
+                for (int col = 0; col < 45; col++)
                 {
-                    // left triangle
-                    GL.Color3(1.0f, 1.0f, 1.0f); GL.Vertex3(-0.6, 0.0f, 4.0f);
-                    GL.Color3(1.0f, 1.0f, 1.0f); GL.Vertex3(0.0, -1.0f, 4.0f);
-                    GL.Color3(1.0f, 1.0f, 1.0f); GL.Vertex3(0.0, 1.0f, 4.0f);
+                    if (row == 0 || row % 2 == 0)
+                    {
+                        if (col % 2 == 1)
+                        {
+                            downVal = -innerRadius();
+                            //drawHex(new Vector3(col * outerRadius() * 1.5f * 2, downVal, 0) + vertOffset, new Vector3(1, 0, 0));
+                        } else
+                        {
+                            drawHex(new Vector3(col * outerRadius() * 1.5f, -innerRadius() * row, 0) + vertOffset, white);
+                        }
+                    }
 
-                    // right triangle
-                    GL.Color3(1.0f, 1.0f, 1.0f); GL.Vertex3(1.6, 0.0f, 4.0f);
-                    GL.Color3(1.0f, 1.0f, 1.0f); GL.Vertex3(1.0, 1.0f, 4.0f);
-                    GL.Color3(1.0f, 1.0f, 1.0f); GL.Vertex3(1.0, -1.0f, 4.0f);
-
-                    // middle left
-                    GL.Color3(1.0f, 1.0f, 1.0f); GL.Vertex3(0, 1.0f, 4.0f);
-                    GL.Color3(1.0f, 1.0f, 1.0f); GL.Vertex3(0.0, -1.0f, 4.0f);
-                    GL.Color3(1.0f, 1.0f, 1.0f); GL.Vertex3(1.0, -1.0f, 4.0f);
-
-                    // middle right
-                    GL.Color3(1.0f, 1.0f, 1.0f); GL.Vertex3(0, 1.0f, 4.0f);
-                    GL.Color3(1.0f, 1.0f, 1.0f); GL.Vertex3(1.0, -1.0f, 4.0f);
-                    GL.Color3(1.0f, 1.0f, 1.0f); GL.Vertex3(1.0, 1.0f, 4.0f);
-
-                    // second hex
-                    // left triangle
-                    GL.Color3(1.0f, .0f, .0f); GL.Vertex3(1, -1.0f, 4.0f);
-                    GL.Color3(1.0f, .0f, .0f); GL.Vertex3(1.6, -2.0f, 4.0f);
-                    GL.Color3(1.0f, .0f, .0f); GL.Vertex3(1.6, 0.0f, 4.0f);
-
-                    GL.Color3(1.0f, .0f, .0f); GL.Vertex3(1.6, 0.0f, 4.0f);
-                    GL.Color3(1.0f, .0f, .0f); GL.Vertex3(1.6, -2.0f, 4.0f);
-                    GL.Color3(1.0f, .0f, .0f); GL.Vertex3(2.6, -2.0f, 4.0f);
+                    if (row % 2 == 1)
+                    {
+                        if (col % 2 == 0)
+                            drawHex(new Vector3((outerRadius() * 1.5f * col) + 1.5f, -innerRadius() * row, 0) + vertOffset, new Vector3(0, 1, 1));
+                    }
                     
-                    GL.Color3(1.0f, .0f, .0f); GL.Vertex3(1.6, 0.0f, 4.0f);
-                    GL.Color3(1.0f, .0f, .0f); GL.Vertex3(2.6, -2.0f, 4.0f);
-                    GL.Color3(1.0f, .0f, .0f); GL.Vertex3(2.6, 0.0f, 4.0f);
 
-                    GL.Color3(1.0f, .0f, .0f); GL.Vertex3(3.2, -1.0f, 4.0f);
-                    GL.Color3(1.0f, .0f, .0f); GL.Vertex3(2.6, 0.0f, 4.0f);
-                    GL.Color3(1.0f, .0f, .0f); GL.Vertex3(2.6, -2.0f, 4.0f);
                 }
             }
-                
+
             GL.End();
-
-            
-
             SwapBuffers();
         }
 
